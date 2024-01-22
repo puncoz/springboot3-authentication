e2169742f7ea2784a5c1879ee7c83aca730e825c
Puncoz Nepal <puncoz138@gmail.com>
2024-01-22 14:10:18 +0900

    with preauthentication

--- src/main/java/com/practice/authentication/config/SecurityConfig.java
+++ src/main/java/com/practice/authentication/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableMethodSecurity
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
@@ -27,10 +30,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
 
-        httpSecurity.authorizeHttpRequests(req ->
-                req.requestMatchers(WHITE_LIST_URLS).permitAll()
-                        .anyRequest().authenticated()
-        );
+//        httpSecurity.authorizeHttpRequests(req ->
+//                req.requestMatchers(WHITE_LIST_URLS).permitAll()
+//                        .anyRequest().authenticated()
+//        );
+        httpSecurity.authorizeHttpRequests(req -> req.anyRequest().permitAll());
 
         httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         httpSecurity.authenticationProvider(authenticationProvider);
--- src/main/java/com/practice/authentication/demo/DemoController.java
+++ src/main/java/com/practice/authentication/demo/DemoController.java
@@ -1,6 +1,7 @@
 package com.practice.authentication.demo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/demo")
 public class DemoController {
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello from secured endpoint");
